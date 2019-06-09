833188d75c3c934598549813f9f2b299db6907e8
juanse-lk <juanselk123@gmail.com>
2019-06-09 12:09:24 -0300

    contar hasta con for

---
+++ java/contarHasta.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class contarHasta {
+
+	public static void main(String[] args) {
+		System.out.println("Ingrese un numero");
+		int num;
+Scanner S = new Scanner(System.in);
+num = S.nextInt();
+
+for(int i=1;i<=num;i++)
+{
+	System.out.println(i);
+	
+}
+	}
+
+}
